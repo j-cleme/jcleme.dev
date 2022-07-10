99496a20c1df6731d4d166670b9b1ff2c176380c
jcleme <clemensjosh@protonmail.com>
2022-07-10 14:41:40 -0400

    removed unused styles

--- src/routes/blog.svelte
+++ src/routes/blog.svelte
@@ -11,21 +11,4 @@
 </center>
 
 <style>
-	header {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		margin-top: 8vh;
-	}
-	h1 {
-		color: var(--color-green);
-		font-size: 120px;
-	}
-	h2 {
-		font-size: 60px;
-	}
-	h2 :nth-child(2) {
-		color: var(--color-green);
-	}
 </style>
