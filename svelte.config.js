e097fca32fcd5466bf775cece68a1c4069a47173
jcleme <clemensjosh@protonmail.com>
2022-08-08 15:52:54 -0400

    Explicitly use adapter-vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			split: false
-		}),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
+		adapter: vercel(),
 
 		// Override http methods in the Todo forms
 		methodOverride: {
